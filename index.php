cb74f12f7b950ef9371098f54324d9492a594722
Marootc <534640202@qq.com>
2016-12-05 11:59:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+Compare changes
+Compare changes across branches, commits, tags, and more below. If you need to, you can also  compare across forks.
+base: master  ...  compare: master 
+ Create pull request
